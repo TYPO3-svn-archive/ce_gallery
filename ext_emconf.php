5b005043c0273c60f1eea0b3aadcd162bcf67f25
cehret <cehret@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-12-09 15:57:23 +0000

    updated for new files...
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ce_gallery/trunk@7434 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "ce_gallery"
 #
-# Auto generated 29-05-2007 18:31
+# Auto generated 09-12-2007 16:49
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:28:{s:9:"ChangeLog";s:4:"d74d";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"f918";s:17:"ext_localconf.php";s:4:"4c5b";s:14:"ext_tables.php";s:4:"2435";s:14:"ext_tables.sql";s:4:"c757";s:19:"flexform_ds_pi1.xml";s:4:"aa1a";s:13:"locallang.xml";s:4:"619b";s:16:"locallang_db.xml";s:4:"6554";s:14:"doc/manual.sxw";s:4:"df77";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:30:"pi1/class.tx_cegallery_pi1.php";s:4:"0035";s:38:"pi1/class.tx_cegallery_pi1_wizicon.php";s:4:"ce05";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"2401";s:24:"pi1/static/constants.txt";s:4:"d41d";s:24:"pi1/static/editorcfg.txt";s:4:"b9b0";s:20:"pi1/static/setup.txt";s:4:"ba9d";s:18:"js/client_sniff.js";s:4:"1054";s:12:"js/moo.fx.js";s:4:"ec2e";s:17:"js/moo.fx.pack.js";s:4:"c805";s:20:"js/prototype.lite.js";s:4:"503c";s:24:"js/showcase.slideshow.js";s:4:"f3ba";s:15:"js/slideshow.js";s:4:"734c";s:21:"js/timed.slideshow.js";s:4:"5c27";s:12:"res/left.gif";s:4:"f6e3";s:13:"res/right.gif";s:4:"e1d5";s:16:"static/setup.txt";s:4:"592d";}',
+	'_md5_values_when_last_written' => 'a:30:{s:9:"ChangeLog";s:4:"e90d";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"d0d1";s:12:"ext_icon.gif";s:4:"f918";s:17:"ext_localconf.php";s:4:"172c";s:14:"ext_tables.php";s:4:"6f2a";s:14:"ext_tables.sql";s:4:"c757";s:19:"flexform_ds_pi1.xml";s:4:"e994";s:13:"locallang.xml";s:4:"619b";s:16:"locallang_db.xml";s:4:"2f91";s:14:"doc/manual.sxw";s:4:"db2e";s:15:"doc/realurl.txt";s:4:"3511";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:30:"pi1/class.tx_cegallery_pi1.php";s:4:"7bf1";s:38:"pi1/class.tx_cegallery_pi1_wizicon.php";s:4:"ce05";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"2401";s:24:"pi1/static/constants.txt";s:4:"d41d";s:24:"pi1/static/editorcfg.txt";s:4:"b9b0";s:20:"pi1/static/setup.txt";s:4:"ba9d";s:18:"js/client_sniff.js";s:4:"1054";s:12:"js/moo.fx.js";s:4:"ec2e";s:17:"js/moo.fx.pack.js";s:4:"ae77";s:20:"js/prototype.lite.js";s:4:"503c";s:24:"js/showcase.slideshow.js";s:4:"9b73";s:15:"js/slideshow.js";s:4:"734c";s:21:"js/timed.slideshow.js";s:4:"5c27";s:12:"res/left.gif";s:4:"f6e3";s:13:"res/right.gif";s:4:"e1d5";s:16:"static/setup.txt";s:4:"e124";}',
 	'suggests' => array(
 	),
 );
