a48533cc488566699536d35e7663378cedb116cd
cehret <cehret@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-12-23 14:30:02 +0000

    new version 2.0.3
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ce_gallery/trunk@7642 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "ce_gallery"
 #
-# Auto generated 20-12-2007 17:07
+# Auto generated 23-12-2007 15:25
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Photogallery with AJAX based Slideshow - uses DAM and DAM categories. Thumbnails are automatically generated and cached. PKM Slimbox can be integrated. Easy configuration.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '2.0.2',
+	'version' => '2.0.3',
 	'dependencies' => 'cms,dam,dam_catedit',
 	'conflicts' => '',
 	'priority' => '',
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:30:{s:9:"ChangeLog";s:4:"e90d";s:10:"README.txt";s:4:"c1df";s:21:"ext_conf_template.txt";s:4:"d0d1";s:12:"ext_icon.gif";s:4:"f918";s:17:"ext_localconf.php";s:4:"2f6a";s:14:"ext_tables.php";s:4:"6f2a";s:14:"ext_tables.sql";s:4:"c757";s:19:"flexform_ds_pi1.xml";s:4:"e3a1";s:13:"locallang.xml";s:4:"619b";s:16:"locallang_db.xml";s:4:"03e9";s:14:"doc/manual.sxw";s:4:"2e50";s:15:"doc/realurl.txt";s:4:"3511";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:30:"pi1/class.tx_cegallery_pi1.php";s:4:"caff";s:38:"pi1/class.tx_cegallery_pi1_wizicon.php";s:4:"ce05";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"6561";s:24:"pi1/static/constants.txt";s:4:"d41d";s:24:"pi1/static/editorcfg.txt";s:4:"b9b0";s:20:"pi1/static/setup.txt";s:4:"1bbb";s:18:"js/client_sniff.js";s:4:"1054";s:12:"js/moo.fx.js";s:4:"ec2e";s:17:"js/moo.fx.pack.js";s:4:"ae77";s:20:"js/prototype.lite.js";s:4:"503c";s:24:"js/showcase.slideshow.js";s:4:"9b73";s:15:"js/slideshow.js";s:4:"734c";s:21:"js/timed.slideshow.js";s:4:"5c27";s:12:"res/left.gif";s:4:"f6e3";s:13:"res/right.gif";s:4:"e1d5";s:16:"static/setup.txt";s:4:"2040";}',
+	'_md5_values_when_last_written' => 'a:30:{s:9:"ChangeLog";s:4:"e90d";s:10:"README.txt";s:4:"c1df";s:21:"ext_conf_template.txt";s:4:"d0d1";s:12:"ext_icon.gif";s:4:"f918";s:17:"ext_localconf.php";s:4:"2f6a";s:14:"ext_tables.php";s:4:"6f2a";s:14:"ext_tables.sql";s:4:"c757";s:19:"flexform_ds_pi1.xml";s:4:"e3a1";s:13:"locallang.xml";s:4:"619b";s:16:"locallang_db.xml";s:4:"03e9";s:14:"doc/manual.sxw";s:4:"7a67";s:15:"doc/realurl.txt";s:4:"3511";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:30:"pi1/class.tx_cegallery_pi1.php";s:4:"ce39";s:38:"pi1/class.tx_cegallery_pi1_wizicon.php";s:4:"ce05";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"6561";s:24:"pi1/static/constants.txt";s:4:"d41d";s:24:"pi1/static/editorcfg.txt";s:4:"b9b0";s:20:"pi1/static/setup.txt";s:4:"1bbb";s:18:"js/client_sniff.js";s:4:"1054";s:12:"js/moo.fx.js";s:4:"ec2e";s:17:"js/moo.fx.pack.js";s:4:"ae77";s:20:"js/prototype.lite.js";s:4:"503c";s:24:"js/showcase.slideshow.js";s:4:"9b73";s:15:"js/slideshow.js";s:4:"734c";s:21:"js/timed.slideshow.js";s:4:"5c27";s:12:"res/left.gif";s:4:"f6e3";s:13:"res/right.gif";s:4:"e1d5";s:16:"static/setup.txt";s:4:"2040";}',
 	'suggests' => array(
 	),
 );
